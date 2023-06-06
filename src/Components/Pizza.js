a1a0ace7228c02d02fa71373dea9c5ea75baf453
fadix1234 <ahmadzaiyasmin@yahoo.com>
2023-06-05 23:10:26 -0700

    yes

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -1,36 +1,64 @@
+import axios from "axios";
+import React, {useState, useEffect} from 'react';
+
 export default function Box() {
+  
+  const [form, setForm] = useState({ name:'', size:'', topping1:'', topping2:'', topping3:'', topping4:'', special:''});
+
+  const submit = event => {
+    debugger
+    event.preventDefault()
+     const newUser = {name: form.name, size: form.size, topping1: form.topping1, topping2: form.topping2, topping3: form.topping3, topping4: form.topping4, special: form.special }
+    axios.post('https://reqres.in/api/orders', newUser)
+    .then(res => {
+      setForm({name:'', size:'', topping1:'', topping2:'', topping3:'', topping4:'', special:''})
+    })
+    .catch(err => {
+    
+    })
+  }
+
     return (
       <div>
         <h1>order-pizza</h1>
 
-        <form>
+        <form onSubmit={submit}>
          <label>name
-         <input id="name-input" type="text"/>
-       </label>
+         <input onChange={evt => setForm(evt.target.value)}
+          id="name-input" type="text"/>
+         </label>
 
-       <label> pizza size
-        <select id="size-dropdown">
+         <label>size
+        <select onChange={evt => setForm(evt.target.value)}
+        id="size-dropdown">
          <option>14 inches</option>
          <option>16 inches</option>
         </select>
        </label>
 
-       <label> onion
-        <input type="checkbox"/>
+       <label> topping1
+        <input onChange={evt => setForm(evt.target.value)}
+        type="checkbox"/>
        </label>
-       <label> mushrooms
-        <input type="checkbox"/>
+       <label> topping2
+        <input onChange={evt => setForm(evt.target.value)}
+        type="checkbox"/>
        </label>
-       <label> chicken
-        <input type="checkbox"/>
+       <label> topping3
+        <input onChange={evt => setForm(evt.target.value)}
+        type="checkbox"/>
        </label>
-       <label> green peppers
-        <input type="checkbox"/>
+       <label> topping4
+        <input onChange={evt => setForm(evt.target.value)}
+         type="checkbox"/>
        </label>
 
-       <label>special instructions
-         <input id="special-text" type="text"/>
+       <label>special 
+         <input onChange={evt => setForm(evt.target.value)}
+        id="special-text" type="text"/>
        </label>
+
+       <button id="order-button">submit</button>
 
       </form>
 
